bb77a98755a9479dd90560a08110902f4f510721
Lee JaeHyun <myleje@gmail.com>
2022-07-26 19:48:06 +0900

    .

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -7,15 +7,13 @@ public class FireCtrl : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform firePos;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Instantiate(생성할 객체, 좌표, 각도)
+            Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+        }
     }
 }

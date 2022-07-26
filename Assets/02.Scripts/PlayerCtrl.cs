1159d050f5654ea7222cae50924144b570f04a1b
Lee JaeHyun <myleje@gmail.com>
2022-07-26 16:37:23 +0900

    Project Init

---
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    private float v;
+    private float h;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        v = Input.GetAxis("Vertical");  // Up/Down  -1.0f ~ 0.0f ~ +1.0f
+        Debug.Log("v=" + v);
+    }
+}

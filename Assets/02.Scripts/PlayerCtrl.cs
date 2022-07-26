dc190d4f7944ec1dd0d9e515a3f822c0dc0b03d2
Lee JaeHyun <myleje@gmail.com>
2022-07-26 16:46:48 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -18,5 +18,7 @@ public class PlayerCtrl : MonoBehaviour
     {
         v = Input.GetAxis("Vertical");  // Up/Down  -1.0f ~ 0.0f ~ +1.0f
         Debug.Log("v=" + v);
+
+        transform.position += new Vector3(0, 0, 0.01f);
     }
 }

ccb0b1a842b9b332f9e1ce07cf512e344de10dc3
Lee JaeHyun <myleje@gmail.com>
2022-07-28 14:16:00 +0900

    .

---
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveBullet : MonoBehaviour
+{
+    /*
+        충돌 콜백함수 (Collision CallBack Function)
+
+        1) 양쪽 다 Collider Component
+        2) 이동하는 객체에는 반드시 Rigidbody Component
+
+        void OnCollisionEnter -> 1회 호출
+        void OnCollisionStay  -> n번 호출
+        void OnCollisionExit  -> 1회 호출 
+
+        Is Trigger 체크된 경우
+
+        void OnTriggerEnter/Stay/Exit
+    */
+
+
+}

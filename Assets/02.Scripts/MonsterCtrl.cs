c5db97f3005c234c521e740a71fccb0a75ced478
Lee JaeHyun <myleje@gmail.com>
2022-07-28 18:17:17 +0900

    .

--- Assets/02.Scripts/MonsterCtrl.cs
+++ Assets/02.Scripts/MonsterCtrl.cs
@@ -13,6 +13,17 @@ public class MonsterCtrl : MonoBehaviour
     // 몬스터의 상태 변수
     public State state = State.IDLE;
 
+    // 추적 사정거리
+    public float traceDist = 10.0f;
+
+    // 공격 사정거리
+    public float attackDist = 2.0f;
+
+    [SerializeField]
+    private Transform monsterTr;
+    [SerializeField]
+    private Transform playerTr;
+
     // Start is called before the first frame update
     void Start()
     {
